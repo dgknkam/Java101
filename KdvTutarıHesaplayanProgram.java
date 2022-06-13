4c3fed281b9da227f4b66fcc042d18b0f461ad4a
Doğukan Kam <83556539+dgknkam@users.noreply.github.com>
2022-06-13 23:15:24 +0300

    Create KdvTutarıHesaplayanProgram.java
    Patika.dev sitesinde pratik olarak yaptığım uygulama

---
+++ KdvTutarıHesaplayanProgram.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // ürünün satış fiyatı = ürünün fiyatı + ürün*(18/100)kdv
+
+        //Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 
+        //Tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan programı yazınız.
+        Scanner input = new Scanner(System.in);
+
+        double kdv = 0.18;
+        double ürününfiyatı = 0;
+
+        System.out.println("Ürünün Fiyatını Girin");
+        ürününfiyatı = input.nextDouble();
+
+        System.out.println("ürünün fiyatı : " + ürününfiyatı);
+        kdv = (0<ürününfiyatı) && (ürününfiyatı<1000) ? 0.18 : 0.081;
+
+        double ürününkdvlifiyatı = (ürününfiyatı * kdv) + ürününfiyatı;
+        System.out.println("ürünün kdvli fiyatı" + ürününkdvlifiyatı);
+
+        double ürününkdvtutarı = ürününkdvlifiyatı-ürününfiyatı;
+        System.out.println("ürünün kdv tutarı" + ürününkdvtutarı);
+        
+
+    }
+}
